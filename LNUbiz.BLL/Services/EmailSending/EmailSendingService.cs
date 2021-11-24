5941705cb8c0db5df6d298d8b6f3007a30b26326
tania <tanyabiryuk7@gmail.com>
2021-11-24 19:07:26 +0200

    Configure confirmation email sending

--- LNUbiz.BLL/Services/EmailSending/EmailSendingService.cs
+++ LNUbiz.BLL/Services/EmailSending/EmailSendingService.cs
@@ -13,10 +13,10 @@ namespace LNUbiz.BLL.Services
     {
         private readonly ILoggerService<EmailSendingService> _loggerService;
 
-        public EmailSendingService(IOptions<EmailServiceSettings> settings,
+        public EmailSendingService(IOptions<EmailServiceSettings>      settings,
                                    ILoggerService<EmailSendingService> loggerService)
         {
-            Settings = settings;
+            Settings       = settings;
             _loggerService = loggerService;
         }
 
@@ -25,19 +25,22 @@ namespace LNUbiz.BLL.Services
         ///<inheritdoc/>
         public async Task<bool> SendEmailAsync(string email, string subject, string message, string title)
         {
-            var SMTPServer = Settings.Value.SMTPServer;
-            var Port = Settings.Value.Port;
-            var SMTPServerLogin = Settings.Value.SMTPServerLogin;
+            var SMTPServer         = Settings.Value.SMTPServer;
+            var Port               = Settings.Value.Port;
+            var SMTPServerLogin    = Settings.Value.SMTPServerLogin;
             var SMTPServerPassword = Settings.Value.SMTPServerPassword;
 
             var emailMessage = new MimeMessage();
             emailMessage.From.Add(new MailboxAddress(title, SMTPServerLogin));
-            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.To  .Add(new MailboxAddress("", email));
+
             emailMessage.Subject = subject;
-            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = message };
+            emailMessage.Body    = new TextPart(MimeKit.Text.TextFormat.Html) { Text = message };
             try
             {
                 using var client = new SmtpClient();
+                client.CheckCertificateRevocation = false;
+
                 await client.ConnectAsync(SMTPServer, Port, true);
                 await client.AuthenticateAsync(SMTPServerLogin, SMTPServerPassword);
                 await client.SendAsync(emailMessage);
