65d4ded00dfb8a7fa329baf998e52567684974eb
tania <tanyabiryuk7@gmail.com>
2021-11-24 19:01:16 +0200

    Add mapping between User and UserDTO

---
+++ LNUbiz.BLL/Mapping/User/UserMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LNUbiz.BLL.DTO.UserProfiles;
+
+namespace LNUbiz.BLL.Mapping.User
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<DAL.Entities.User, UserDTO>().ReverseMap();
+        }
+    }
+}
