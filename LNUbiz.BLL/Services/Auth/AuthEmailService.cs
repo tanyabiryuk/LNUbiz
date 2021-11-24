5e30edce155213f4400624a7df41cb7de400aa08
tania <tanyabiryuk7@gmail.com>
2021-11-24 19:15:56 +0200

    Update user EmailSendedOnRegister date

--- LNUbiz.BLL/Services/Auth/AuthEmailService.cs
+++ LNUbiz.BLL/Services/Auth/AuthEmailService.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Routing;
-using NLog.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 using LNUbiz.BLL.DTO.Account;
@@ -15,35 +14,35 @@ namespace LNUbiz.BLL.Services.Auth
     public class AuthEmailService : IAuthEmailService
     {
         private readonly IActionContextAccessor _actionContextAccessor;
-        private readonly IAuthService _authService;
-        private readonly IHttpContextAccessor _contextAccessor;
-        private readonly IEmailSendingService _emailSendingService;
-        private readonly IEmailContentService _emailContentService;
-        private readonly IUrlHelperFactory _urlHelperFactory;
-        private readonly UserManager<User> _userManager;
+        private readonly IAuthService           _authService;
+        private readonly IHttpContextAccessor   _contextAccessor;
+        private readonly IEmailSendingService   _emailSendingService;
+        private readonly IEmailContentService   _emailContentService;
+        private readonly IUrlHelperFactory      _urlHelperFactory;
+        private readonly UserManager<User>      _userManager;
 
-        public AuthEmailService(IEmailSendingService emailSendingService,
-                                IEmailContentService emailContentService,
-                                IAuthService authService,
-                                UserManager<User> userManager,
-                                IUrlHelperFactory urlHelperFactory,
+        public AuthEmailService(IEmailSendingService   emailSendingService,
+                                IEmailContentService   emailContentService,
+                                IAuthService           authService,
+                                UserManager<User>      userManager,
+                                IUrlHelperFactory      urlHelperFactory,
                                 IActionContextAccessor actionContextAccessor,
-                                IHttpContextAccessor contextAccessor)
+                                IHttpContextAccessor   contextAccessor)
         {
             _emailSendingService = emailSendingService;
             _emailContentService = emailContentService;
-            _authService = authService;
-            _userManager = userManager;
+            _authService         = authService;
+            _userManager         = userManager;
 
-            _contextAccessor = contextAccessor;
-            _urlHelperFactory = urlHelperFactory;
+            _contextAccessor       = contextAccessor;
+            _urlHelperFactory      = urlHelperFactory;
             _actionContextAccessor = actionContextAccessor;
         }
 
         /// <inheritdoc />
         public async Task<IdentityResult> ConfirmEmailAsync(string userId, string code)
         {
-            var user = await _userManager.FindByIdAsync(userId);
+            var user   = await _userManager.FindByIdAsync(userId);
             var result = await _userManager.ConfirmEmailAsync(user, code);
             return result;
         }
@@ -51,27 +50,34 @@ namespace LNUbiz.BLL.Services.Auth
         /// <inheritdoc />
         public async Task<bool> SendEmailGreetingAsync(string email)
         {
-            var citiesUrl = ConfigSettingLayoutRenderer.DefaultConfiguration.GetSection("URLs")["Cities"];
-            citiesUrl = citiesUrl.Remove(citiesUrl.Length - 1);
-            var user = await _userManager.FindByEmailAsync(email);
-            user.EmailSendedOnRegister = DateTime.Now;
+            var user         = await _userManager.FindByEmailAsync(email);
             var emailContent = _emailContentService.GetAuthGreetingEmail();
-            return await _emailSendingService.SendEmailAsync(user.Email, emailContent.Subject, emailContent.Message, emailContent.Title);
+            return await _emailSendingService
+                             .SendEmailAsync( user.Email
+                                            , emailContent.Subject
+                                            , emailContent.Message
+                                            , emailContent.Title);
         }
 
         /// <inheritdoc />
         public async Task<bool> SendEmailRegistrAsync(string email)
         {
-            IUrlHelper _url = _urlHelperFactory.GetUrlHelper(_actionContextAccessor.ActionContext);
-            var token = await _authService.AddRoleAndTokenAsync(email);
-            var user = await _userManager.FindByEmailAsync(email);
+            IUrlHelper _url      = _urlHelperFactory.GetUrlHelper(_actionContextAccessor.ActionContext);
+            var token            = await _authService.AddRoleAndTokenAsync(email);
+            var user             = await _userManager.FindByEmailAsync(email);
             var confirmationLink = _url.Action("confirmingEmail",
                                                "Auth",
                                                new { token, userId = user.Id },
                                                _contextAccessor.HttpContext.Request.Scheme);
             user.EmailSendedOnRegister = DateTime.Now;
-            var emailContent = _emailContentService.GetAuthRegisterEmail(confirmationLink);
-            return await _emailSendingService.SendEmailAsync(email, emailContent.Subject, emailContent.Message, emailContent.Title);
+            var emailContent           = _emailContentService.GetAuthRegisterEmail(confirmationLink);
+            return await _emailSendingService
+                             .SendEmailAsync( email
+                                            , emailContent.Subject
+                                            , emailContent.Message
+                                            , emailContent.Title)
+                 ? (await _userManager.UpdateAsync(user)).Succeeded 
+                 : false;
         }
 
         /// <inheritdoc />
@@ -81,7 +87,10 @@ namespace LNUbiz.BLL.Services.Auth
             user.EmailSendedOnForgotPassword = DateTime.Now;
             await _userManager.UpdateAsync(user);
             var emailContent = _emailContentService.GetAuthResetPasswordEmail(confirmationLink);
-            await _emailSendingService.SendEmailAsync(forgotPasswordDto.Email, emailContent.Subject, emailContent.Message, emailContent.Title);
+            await _emailSendingService.SendEmailAsync( forgotPasswordDto.Email
+                                                     , emailContent.Subject
+                                                     , emailContent.Message
+                                                     , emailContent.Title);
         }
     }
 }
